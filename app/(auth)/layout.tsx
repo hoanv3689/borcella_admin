126cbb5915b5fbe55c34b14b25230c688d650b19
hoanv-3241 <nguyen.van.hoa@sun-asterisk.com>
2024-11-09 15:06:31 +0700

    init & config auth start

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,32 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Borcelle - Admin Auth",
+  description: "Admin dashboard to manage Borcelle's data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
